5f9a0648aca2ee211edb9d1ff5e59e8e4ffa878c
Sébastien <apbseb@hotmail.fr>
2022-01-24 23:21:44 +0100

    chore: add new test + minor improvements

--- __tests/features/bigparsing_confirmed.spec.ts
+++ __tests/features/bigparsing_confirmed.spec.ts
@@ -1,12 +1,17 @@
 import { BigParsingService } from "@features/bigparsing_confirmed/services";
 
 describe("Big parsing confirmed", () => {
-  jest.setTimeout(15000);
+  jest.setTimeout(20000);
 
-  it("should returns an user", async () => {
-    const bigParsingService = new BigParsingService();
+  const bigParsingService = new BigParsingService();
+
+  it("should return an user", async () => {
     const user = await bigParsingService.searchUserByID({ id: 62359 });
-
     expect(user.name).toEqual("Damon Jerde");
+  });
+
+  it("should return user not found with null", async () => {
+    const user = await bigParsingService.searchUserByID({ id: 1234567890 });
+    expect(user).toBeNull();
   });
 });

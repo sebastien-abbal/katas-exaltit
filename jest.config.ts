c3e7ac61ffb2e358884cc352087bf8650270bc3b
Sébastien <apbseb@hotmail.fr>
2022-01-25 12:46:32 +0100

    chore: rename types folder to @types

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ module.exports = {
   moduleNameMapper: {
     "^@features/(.*)": "<rootDir>/src/features/$1",
     "^@utils": "<rootDir>/src/utils",
-    "^@types": "<rootDir>/src/types",
+    "^@types": "<rootDir>/src/@types",
     "^@config": "<rootDir>/src/config",
   },
   testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
